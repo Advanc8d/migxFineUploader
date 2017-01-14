7ba0f38bfbb187eaa01b3a627d3ae6d2646dfee8
Bruno17 <b.perner@gmx.de>
2017-01-14 13:49:03 +0100

    package 0.2.0-beta1

---
+++ core/components/migxfineuploader/lexicon/en/default.inc.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Default Lexicon Entries for migxFineUploader
+ *
+ * @package migxfineuploader
+ * @subpackage lexicon
+ */
+$_lang['mfu'] = 'migxFineUploader';
+$_lang['mfu.notWritable'] = "Server error. Upload directory isn't writable.";
+$_lang['mfu.targetNotWritable'] = "Could not copy the uploaded file to the target folder. Does the folder exist and is it writable?";
+$_lang['mfu.noFile'] = "No files were uploaded.";
+$_lang['mfu.emptyFile'] = "File is empty.";
+$_lang['mfu.largeFile'] = "File is too large.";
+$_lang['mfu.wrongExtension'] = "File has an invalid extension, it should be one of [[+allowedExtensions]].";
+$_lang['mfu.saveError'] = "Could not save uploaded file. The upload was cancelled or a server error encountered.";
+$_lang['mfu.maxFiles'] = "Only [[+maxFiles]] different files are allowed to upload.";
+$_lang['mfu.notFound'] = "File is not found.";
+$_lang['mfu.thumbnailGenerationProblem'] = "Thumbnail generation failed.";
+$_lang['mfu.dropArea'] = "Drop files here to upload.";
+$_lang['mfu.uploadButton'] = "Upload";
+$_lang['mfu.deleteButton'] = "Delete";
+$_lang['mfu.clearButton'] = "Delete all";
+$_lang['mfu.cancel'] = "Cancel";
+$_lang['mfu.failed'] = "Failed";
+$_lang['mfu.typeError'] = "Unfortunately the file(s) you selected weren't the type we were expecting. Only {extensions} files are allowed.";
+$_lang['mfu.sizeError'] = "{file} is too large, maximum file size is {sizeLimit}.";
+$_lang['mfu.minSizeError'] = "{file} is too small, minimum file size is {minSizeLimit}.";
+$_lang['mfu.emptyError'] = "{file} is empty, please select files again without it.";
+$_lang['mfu.onLeave'] = "The files are being uploaded, if you leave now the upload will be cancelled.";
+
+$_lang['mfu.retry'] = "Retry";
