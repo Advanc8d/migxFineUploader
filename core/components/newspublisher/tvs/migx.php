7fe64c0612b41ae18f03ab10294b004f07bbb87d
Bruno17 <b.perner@gmx.de>
2017-01-14 20:20:15 +0100

    np - MIGX-tv mod

--- core/components/newspublisher/tvs/migx.php
+++ core/components/newspublisher/tvs/migx.php
@@ -3,4 +3,4 @@
 $formTpl .= '<div style="display:none;">';
 $formTpl .= $this->_displaySimple($name, 'TextAreaTpl', $this->textMaxlength);
 $formTpl .= '</div>';
-$formTpl .= '[[!migxFineUploader? &tvname=`'.$name.'` &addJquery=`0` &debug=`0` &maxFiles=`10`]]';
+$formTpl .= '[[!migxFineUploader? &tvname=`'.$name.'` ]]';
